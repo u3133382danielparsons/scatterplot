4707e56f10cb22e6698ed9d3dd3b540ec672a71a
Daniel Parsons <danielparsons3133382@gmail.com>
2019-09-28 00:05:37 +1000

    Bringing scatterplot data from firestoreio.com

--- src/App.js
+++ src/App.js
@@ -3,10 +3,28 @@ import Navbar from 'react-bootstrap/Navbar';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
+import { json } from 'd3';
 
 import ChartWrapper from './ChartWrapper';
 
 class App extends Component {
+  state = {
+    data: []
+  }
+
+  componentWillMount() {
+    json('https://udemy-react-d3.firebaseio.com/children.json')
+      .then(data => this.setState({ data }))
+      .catch(error => console.log(error));
+  }
+
+  renderChart() {
+    if (this.state.data.length === 0) {
+      return "No data yet"
+    } 
+    return <ChartWrapper data={this.state.data} />
+  }
+
   render() {
     return (
       <div>
@@ -15,7 +33,7 @@ class App extends Component {
         </Navbar>
         <Container>
           <Row>
-            <Col md={6} xs={12}><ChartWrapper /></Col>
+            <Col md={6} xs={12}>{this.renderChart()}</Col>
             <Col md={6} xs={12}></Col>
           </Row>
         </Container>
