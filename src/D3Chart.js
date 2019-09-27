2e7e4a3f8156bebc4770006fa594f2cbb650326f
Daniel Parsons <danielparsons3133382@gmail.com>
2019-09-28 00:56:40 +1000

    Added the axes and labels

--- src/D3Chart.js
+++ src/D3Chart.js
@@ -8,7 +8,8 @@ class D3Chart {
 	constructor(element, data) {
 		let vis = this;
 
-		console.log(data);
+		vis.data = data;
+
 		vis.g = d3.select(element)
 			.append("svg")
 				.attr("width", WIDTH + MARGIN.LEFT + MARGIN.RIGHT)
@@ -16,11 +17,66 @@ class D3Chart {
 			.append("g")
 				.attr("transform", `translate(${MARGIN.LEFT}, ${MARGIN.TOP})`);
 
+			vis.x = d3.scaleLinear()
+			   .range([0, WIDTH]);
+
+			vis.y = d3.scaleLinear()
+			   .range([HEIGHT , 0]);
+
+			vis.xAxisGroup = vis.g.append("g")
+				.attr("transform", `translate(0, ${HEIGHT})`);
+
+			vis.yAxisGroup = vis.g.append("g")
+
+			vis.g.append("text")
+				.attr("x", WIDTH / 2)
+				.attr("y", HEIGHT + 40)
+				.attr("font-size", 20)
+				.attr("text-anchor", "middle")
+				.text("Age")
+
+			vis.g.append("text")
+				.attr("x", -(HEIGHT / 2))
+				.attr("y", -50)
+				.attr("transform", "rotate(-90)")
+				.attr("font-size", 20)
+				.attr("text-anchor", "middle")
+				.text("Height in cm")
+
 		vis.update();		
 	}
 
 	update() {
 		let vis = this;
+		vis.x.domain([0, d3.max(vis.data, d => Number(d.age))]);
+		vis.y.domain([0, d3.max(vis.data, d => Number(d.height))]);
+
+		const xAxisCall = d3.axisBottom(vis.x);
+		const yAxisCall = d3.axisLeft(vis.y);
+
+		vis.xAxisGroup.call(xAxisCall);
+		vis.yAxisGroup.call(yAxisCall);
+
+		// JOIN
+		const circles = vis.g.selectAll("circle")
+			.data(vis.data, d => d.name);
+
+		
+
+		// EXIT
+		circles.exit().remove();
+
+		// UPDATE
+		circles
+			.attr("cx", d => vis.x(d.age))
+			.attr("cy", d => vis.y(d.height));
+
+		// ENTER
+		circles.enter().append("circle")
+			.attr("cx", d => vis.x(d.age))
+			.attr("cy", d => vis.y(d.height))
+			.attr("r", 5)
+			.attr("fill", "grey");
 	
 	}
 }
